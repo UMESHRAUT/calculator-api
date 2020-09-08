d1113da21b36c24413484edf61fc73950ace822f
UMESHRAUT <rautumesh34@gmail.com>
2020-09-08 19:43:01 +0530

    working fine

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
 app.get('/',(req,res)=>{
-    res.status(200).json("Hello world!")
+    const hello="Hello world!";
+    console.log(typeof hello);
+    res.status(200).json(hello)
 })
 
 app.post('/add',(req,res)=>{
@@ -46,14 +48,22 @@ app.post('/sub',(req,res)=>{
         n1=Math.min(num1,num2);
         n2=Math.max(num1,num2);
         sum=Number((n1-n2).toFixed(2));
+        console.log("from up");
+        if(sum<-1000000){
+            res.status(200).json({'message':'Underflow'})
+        }
+        res.status(200).json({'message':'the difference of given two numbers','sum':sum})
+
+
     }else{
         n1=Math.max(num1,num2);
         n2=Math.min(num1,num2);
         sum=Number((n1-n2).toFixed(2));
+        console.log("from down");
     }
 
     if(!Number.isInteger(num1) && !Number.isInteger(num2)){
-        res.status(200).json({'message':'the difference of given two numbers','difference':2.0})
+        res.status(200).json({'message':'the difference of given two numbers','sum':2.0})
     }
     // if(Number.isInteger(num1) && Number.isInteger(num2)){
     //     sum=parseInt(sum);
@@ -65,6 +75,7 @@ app.post('/sub',(req,res)=>{
     if(sum<-1000000){
         res.status(200).json({'message':'Underflow'})
     }
+
     res.status(200).json({'message':'the difference of given two numbers','difference':sum})
 })
 
@@ -74,7 +85,7 @@ app.post('/multiply',(req,res)=>{
     const sum=num1*num2
 
     if(typeof num1!="number" || typeof num2!="number"){
-        res.status(200).json({'message':'invalid data types'})
+        res.status(200).json({'message':'Invalid data types'})
     }
     if(sum>1000000){
         res.status(200).json({'message':'Overflow'})
@@ -90,7 +101,7 @@ app.post('/divide',(req,res)=>{
     if(num1==0 || num2==0){
         res.status(200).json({'message':"Cannot divide by zero"})
     }
-    res.status(200).json({'message':'The product of given numbers','result':sum})
+    res.status(200).json({'message':'The division of given numbers','result':sum})
 })
 // here
 
